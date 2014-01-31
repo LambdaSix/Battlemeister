8177f657031ad5c036d630653171ef7eca89f311
LambdaSix <LambdaSix@users.noreply.github.com>
2014-01-31 17:29:17 +0000

    Add unit test for Auction API

---
+++ Battlemeister.Tests/Tests/AuctionTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Battlemeister.AuctionInformation;
+using NUnit.Framework;
+
+namespace Battlemeister.Tests
+{
+    [TestFixture]
+    public class AuctionTests
+    {
+         
+        [Test]
+        public void AuctionApi()
+        {
+            var auction = new AuctionApi(new DefaultConfig());
+
+            var result = auction.Get("medivh");
+            Assert.That(result.Alliance.Auctions.Count() != 0);
+        }
+    }
+}
